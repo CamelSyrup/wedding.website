3008f5e18614de19a5b807d1ba601a2660fc9157
CamelSyrup <joseph@minchellas.com>
2024-08-16 16:51:34 +0100

    Update flipclock.js

--- flipclock.js
+++ flipclock.js
@@ -1,82 +1,113 @@
-// Billboard FlipClock JavaScript
-class BillboardFlipClock {
-    constructor(element, endTime) {
-        this.element = element;
-        this.endTime = new Date(endTime).getTime();
-        this.initClock();
-        this.startCountdown();
+console.clear();
+
+
+function CountdownTracker(label, value){
+
+  var el = document.createElement('span');
+
+  el.className = 'flip-clock__piece';
+  el.innerHTML = '<b class="flip-clock__card card"><b class="card__top"></b><b class="card__bottom"></b><b class="card__back"><b class="card__bottom"></b></b></b>' + 
+    '<span class="flip-clock__slot">' + label + '</span>';
+
+  this.el = el;
+
+  var top = el.querySelector('.card__top'),
+      bottom = el.querySelector('.card__bottom'),
+      back = el.querySelector('.card__back'),
+      backBottom = el.querySelector('.card__back .card__bottom');
+
+  this.update = function(val){
+    val = ( '0' + val ).slice(-2);
+    if ( val !== this.currentValue ) {
+      
+      if ( this.currentValue >= 0 ) {
+        back.setAttribute('data-value', this.currentValue);
+        bottom.setAttribute('data-value', this.currentValue);
+      }
+      this.currentValue = val;
+      top.innerText = this.currentValue;
+      backBottom.setAttribute('data-value', this.currentValue);
+
+      this.el.classList.remove('flip');
+      void this.el.offsetWidth;
+      this.el.classList.add('flip');
     }
-
-    initClock() {
-        this.clockElement = document.createElement('div');
-        this.clockElement.className = 'flipclock';
-        this.element.appendChild(this.clockElement);
-
-        this.createDigits();
-    }
-
-    createDigits() {
-        this.daysElement = this.createDigitElement('Days');
-        this.hoursElement = this.createDigitElement('Hours');
-        this.minutesElement = this.createDigitElement('Minutes');
-        this.secondsElement = this.createDigitElement('Seconds');
-    }
-
-    createDigitElement(label) {
-        const digitContainer = document.createElement('div');
-        digitContainer.className = 'flip-digit';
-
-        const digitTop = document.createElement('div');
-        digitTop.className = 'digit-top';
-        digitContainer.appendChild(digitTop);
-
-        const digitBottom = document.createElement('div');
-        digitBottom.className = 'digit-bottom';
-        digitContainer.appendChild(digitBottom);
-
-        const digitLabel = document.createElement('div');
-        digitLabel.className = 'digit-label';
-        digitLabel.textContent = label;
-        digitContainer.appendChild(digitLabel);
-
-        this.clockElement.appendChild(digitContainer);
-
-        return { top: digitTop, bottom: digitBottom };
-    }
-
-    startCountdown() {
-        this.updateClock();
-        setInterval(() => {
-            this.updateClock();
-        }, 1000);
-    }
-
-    updateClock() {
-        const now = new Date().getTime();
-        const timeRemaining = this.endTime - now;
-
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-        this.setDigit(this.daysElement, days);
-        this.setDigit(this.hoursElement, hours);
-        this.setDigit(this.minutesElement, minutes);
-        this.setDigit(this.secondsElement, seconds);
-    }
-
-    setDigit(digitElement, value) {
-        const paddedValue = String(value).padStart(2, '0');
-
-        digitElement.top.textContent = paddedValue;
-        digitElement.bottom.textContent = paddedValue;
-    }
+  }
+  
+  this.update(value);
 }
 
-// Initialize the FlipClock
-document.addEventListener('DOMContentLoaded', function () {
-    const clockElement = document.querySelector('.flipclock');
-    const endTime = 'Aug 10, 2025 00:00:00';
-    new BillboardFlipClock(clockElement, endTime);
-});
+// Calculation adapted from https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  return {
+    'Total': t,
+    'Days': Math.floor(t / (1000 * 60 * 60 * 24)),
+    'Hours': Math.floor((t / (1000 * 60 * 60)) % 24),
+    'Minutes': Math.floor((t / 1000 / 60) % 60),
+    'Seconds': Math.floor((t / 1000) % 60)
+  };
+}
+
+function getTime() {
+  var t = new Date();
+  return {
+    'Total': t,
+    'Hours': t.getHours() % 12,
+    'Minutes': t.getMinutes(),
+    'Seconds': t.getSeconds()
+  };
+}
+
+function Clock(countdown,callback) {
+  
+  countdown = countdown ? new Date(Date.parse(countdown)) : false;
+  callback = callback || function(){};
+  
+  var updateFn = countdown ? getTimeRemaining : getTime;
+
+  this.el = document.createElement('div');
+  this.el.className = 'flip-clock';
+
+  var trackers = {},
+      t = updateFn(countdown),
+      key, timeinterval;
+
+  for ( key in t ){
+    if ( key === 'Total' ) { continue; }
+    trackers[key] = new CountdownTracker(key, t[key]);
+    this.el.appendChild(trackers[key].el);
+  }
+
+  var i = 0;
+  function updateClock() {
+    timeinterval = requestAnimationFrame(updateClock);
+    
+    // throttle so it's not constantly updating the time.
+    if ( i++ % 10 ) { return; }
+    
+    var t = updateFn(countdown);
+    if ( t.Total < 0 ) {
+      cancelAnimationFrame(timeinterval);
+      for ( key in trackers ){
+        trackers[key].update( 0 );
+      }
+      callback();
+      return;
+    }
+    
+    for ( key in trackers ){
+      trackers[key].update( t[key] );
+    }
+  }
+
+  setTimeout(updateClock,500);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 12 * 24 * 60 * 60 * 1000);
+var c = new Clock(deadline, function(){ alert('countdown complete') });
+document.body.appendChild(c.el);
+
+var clock = new Clock();
+document.body.appendChild(clock.el);
